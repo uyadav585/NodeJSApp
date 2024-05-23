ef739d5b9466de7675593c009efc73b1630ab28e
Kumar Umang <uyadav585@gmail.com>
2024-05-23 21:54:14 +0530

    last checkup

--- mongoC.js
+++ mongoC.js
@@ -5,13 +5,14 @@ dotenv.config();
 
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+console.log(password);
 const connectionString = `mongodb+srv://vk9338530:${password}@cluster0.8yfkcl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; // clustore url mongo with correct pass
 
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
+  console.log("connection successful");
 } catch(e) {
   console.error(e);
 }
