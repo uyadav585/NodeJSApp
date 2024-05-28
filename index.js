bd67e10fef18debf2e39c9b36328ba13bb1a8560
Kumar Umang <uyadav585@gmail.com>
2024-05-28 23:56:15 +0530

    Final and running Pipeline

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
 import db from "./mongoC.js";
+
 
 const port = 4000;
 const app = express();
 
+
+// Middleware to handle CORS
 app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
-  
+    
     next();
   });
 
+
 // Parses the text as url encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
+
 // Parses the text as json
 app.use(bodyParser.json());
 
+
+// Default route
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
 })
 
+
+
+// Route to add a user
 app.post('/addUser',async (req, res) => {
     let collection = await db.collection("users");
     let newDocument = req.body;
@@ -32,6 +41,9 @@ app.post('/addUser',async (req, res) => {
     res.send(result).status(204);
 });
 
+
+// Route to get all users
+
 app.get('/getUsers', async(req, res) => {
     let collection = await db.collection("users");
     let results = await collection.find({})
@@ -40,6 +52,8 @@ app.get('/getUsers', async(req, res) => {
     res.send(results).status(200);
 });
 
+
+// Start the server
 app.listen(port, function () {
     console.log("Server is listening at port:" + port);
 });
--- mongoC.js
+++ mongoC.js
@@ -1,8 +1,19 @@
 import { MongoClient } from "mongodb";
 
+
+// Encode the MongoDB password from the environment variable
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+
+
+// Connection string for MongoDB Atlas cluster
 const connectionString = `mongodb+srv://vk9338530:${password}@cluster0.8yfkcl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; // clustore url
+
+
+// Create a new MongoClient instance
 const client = new MongoClient(connectionString);
+
+
+// Attempting to connect to the MongoDB cluster
 let conn;
 try {
   conn = await client.connect();
@@ -10,5 +21,11 @@ try {
 } catch(e) {
   console.error(e);
 }
+
+
+// Select the specific database ("IPL-Points-Table") to use
 let db = conn.db("IPL-Points-Table");
+
+
+// Export the database connection for use in other modules
 export default db;
